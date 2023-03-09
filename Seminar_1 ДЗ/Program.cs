f6dc1b8504f02137c84e703f51f9fc8b5eb04eba
Raisa Baranova <raisa031084@gmail.com>
2023-03-09 15:20:10 +0300

    Add two task

---
+++ Seminar_1 ДЗ/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа
+//  и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int number2 =Convert.ToInt32(Console.ReadLine());
+// int max=number1;
+
+// if (number1>number2) max= number1;
+// if (number1<number2) max= number2;
+
+// Console.Write ("max=");
+// Console.WriteLine(max);
+
+
+// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int b =Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int c =Convert.ToInt32(Console.ReadLine());
+// int max=a;
+
+// if (a>max) max = a;
+// if (b>max) max= b;
+// if (c>max) max= c;
+
+// Console.WriteLine(max);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдает, 
+// является ли число четным (делится ли оно на два без остатка).
+
+
+    
+
+
