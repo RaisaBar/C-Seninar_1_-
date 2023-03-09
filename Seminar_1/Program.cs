d127f1d935998b13f8f1e21ded2abc141fe3f79c
Raisa Baranova <raisa031084@gmail.com>
2023-03-09 13:37:42 +0300

    Seminar1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача №1.Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+
+
+// Console.WriteLine("Введите текст");
+// int number =Convert.ToInt32(Console.ReadLine());
+// int sqt = number*number;
+// Console.WriteLine(sqt);
+
+
+// Задача №2. Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго.
+
+// Console.WriteLine("Введите число");
+// int number1 =Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int number2 =Convert.ToInt32(Console.ReadLine());
+
+// if( number1==number2*number2)
+// {
+//     Console.WriteLine("Является");
+// }
+// else
+// {
+//     Console.WriteLine("Не является");
+// }
+
+
+// Задача №3. Напишите программу, которая будет выдавать 
+// название дня недели по заданному номеру.
+
+// Console.WriteLine("Введите число");
+// int number1 =Convert.ToInt32(Console.ReadLine());
+
+// if(number1==1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if(number1==2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if(number1==3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if(number1==4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if(number1==5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if(number1==6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if(number1==7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Такого дня недели у нас нет");
+// }
+
+
+// Задача №4. Напишите программу, которая на вход принимает одно число (N),
+//  а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// int numberNeg=-number1;
+
+// while(numberNeg<=number1)
+
+// {
+//     Console.WriteLine(numberNeg);
+//     numberNeg=numberNeg+1;
+// }
+
+
+// Задача №5. Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// int number2=number1%10;
+
+// Console.WriteLine(number2);
+
