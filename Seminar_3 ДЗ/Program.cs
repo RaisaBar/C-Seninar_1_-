9992b5d1b0118e15d487102f540fbb06bb5db5ff
Raisa Baranova <raisa031084@gmail.com>
2023-03-15 17:34:22 +0300

    add tasks seminar 3

---
+++ Seminar_3 ДЗ/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+
+// Console.WriteLine("Введите пятизначное число");
+// string Number= Console.ReadLine ();
+
+// if (Number[0]==Number[4]&&Number[1]==Number[3] )
+// {
+//     Console.WriteLine("число является палиндромом");
+// }
+// else {
+//     Console.WriteLine("не является палиндромом");
+// }
+
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+
+// Console.Write("Введи X точки B");
+// int XB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введи Y точки B");
+// int YB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введи Z точки B");
+// int ZB = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введи X точки A");
+// int XA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введи Y точки A");
+// int YA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введи Z точки A");
+// int ZA = Convert.ToInt32(Console.ReadLine());
+
+// double number =Math.Sqrt((Math.Pow ((XB-XA),2))+(Math.Pow ((YB-YA),2))+(Math.Pow ((ZB-ZA),2)));
+
+//  Console.Write(number);
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int i=1;
+
+// while (i<=number)
+// {
+//     Console.Write(Math.Pow(i,3)+"  ");
+//     ++i;
+// }
