4776e394919427d7807ab31024384321aad296fb
Raisa Baranova <raisa031084@gmail.com>
2023-03-21 14:46:13 +0300

    add 2 tasks

---
+++ Seminar_4 ДЗ/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("Введите число А");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число В");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write(Math.Pow(A,B));
+
+
+
+// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.WriteLine("Введите число");
+// string Number= Console.ReadLine ();
+// int num= number.Length;
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] array = {1,2,5,7,19};
+// void PrintArray (int[] array)
+// {
+//     int count=array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.WriteLine($"{array[i]}");
+//     }
+//     Console.WriteLine();
+// }
+
+// PrintArray(array);
